8a6e37808aaf1f24e98450ec1cc1c1689199c49a
Rafael Freitas <rafontas@gmail.com>
2020-06-24 15:32:36 -0300

    Cartinf 01 - Funcional
        1. Removido digito zero no número do arquivo, depois do char A.
        2. Removido digito zero após primeiro grupo de espaços, se houver.

---
+++ Orquestrador.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace IBGECart
+{
+    public class Orquestrador
+    {
+        public IDictionary<int, string> Arquivos { get; private set; }
+        public IList<TipoArquivo> ArquivosDisponiveis { get; private set; }
+
+        public Orquestrador()
+        {
+            this.ArquivosFactory();
+        }
+
+        private void ArquivosFactory()
+        {
+            this.Arquivos = new Dictionary<int, string>();
+            this.Arquivos.Add(new KeyValuePair<int, string>((int)TipoArquivo.Cartinf01, "CARTINF01.TXT"));
+        }
+
+        public IList<TipoArquivo> VerificarSeHaArquivosDisponiveis()
+        {
+            IList<TipoArquivo> ArquivosDisponiveis = new List<TipoArquivo>();
+
+            foreach(var a in this.Arquivos)
+            {
+                //Console.WriteLine(Directory.GetCurrentDirectory() + "\\" + a.Value);
+
+                if (File.Exists(Directory.GetCurrentDirectory() + "\\" + a.Value))
+                    ArquivosDisponiveis.Add((TipoArquivo) a.Key);
+            }
+
+            this.ArquivosDisponiveis = ArquivosDisponiveis ?? null;
+            return ArquivosDisponiveis;
+        }
+
+        public void CorrigirArquivosDisponiveis() 
+        {
+            if (this.ArquivosDisponiveis.Count <= 0) {
+                Console.WriteLine("Erro: Correção pedida sem arquivos disponíveis.");
+                return;
+            }
+
+            foreach(int arqCorrecao in this.ArquivosDisponiveis) 
+            {
+                switch(arqCorrecao)
+                {
+                    case (int) TipoArquivo.Cartinf01:
+                        Cartinf01 c = new Cartinf01();
+                        Console.Write("    " + c.NOME_ARQUIVO + " - Em andamento...");
+                        c.CorrigeArquivo();
+                        Console.SetCursorPosition(Console.CursorLeft - " Em andamento...".Length, Console.CursorTop);
+                        Console.Write(" Concluído.            " + Environment.NewLine + Environment.NewLine);
+                        Thread.Sleep(10);
+                        break;
+
+                    default: 
+                        Console.WriteLine("Erro: não há existe o arquivo pedido - " + arqCorrecao);
+                        break;
+
+
+                }
+            }
+        }
+    }
+
+    public enum TipoArquivo
+    {
+        Cartinf01 = 1,
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace IBGECart
+{
+    public class Program
+    {
+
+        private static int counter;
+
+        public static void Turn()
+        {
+            counter++;
+            switch (counter % 4)
+            {
+                case 0: Console.Write("/"); counter = 0; break;
+                case 1: Console.Write("-"); break;
+                case 2: Console.Write("\\"); break;
+                case 3: Console.Write("|"); break;
+            }
+            Thread.Sleep(1);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+        }
+
+        public static string ComoUsar()
+        {
+            string comoUsar = @"Instruções de uso:" + Environment.NewLine + Environment.NewLine;
+            comoUsar += @"  Coloque na mesma pasta este arquivo executável e o(s) arquivo(s) que você quer corrigir." + Environment.NewLine;
+            comoUsar += @"  Eles tem que estar EXATAMENTE com um dos nomes abaixo: " + Environment.NewLine;
+
+            return comoUsar;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao IBGE Cartinf Util!" + Environment.NewLine);
+
+            Orquestrador orquestrador = new Orquestrador();
+            IList<TipoArquivo> ArquivosDisponiveis = orquestrador.VerificarSeHaArquivosDisponiveis();
+
+            if (ArquivosDisponiveis.Count > 0)
+            {
+                Console.WriteLine("Foram identificado os arquivos: " + Environment.NewLine);
+                foreach (var f in orquestrador.Arquivos)
+                {
+                    Console.WriteLine("   " + f.Value);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Deseja prosseguir e ajeitar todos eles? Pressione S para sim ou N para não, depois aperte Enter.");
+
+                string consoleInput = Console.ReadLine();
+
+                if (consoleInput.ToLower() == "sim" || consoleInput.ToLower() == "s")
+                {
+                    Console.WriteLine("Corrigindo os arquivos. Isso pode demorar alguns minutos." + Environment.NewLine);
+                    orquestrador.CorrigirArquivosDisponiveis();
+
+                }
+                else if (consoleInput.ToLower() != "nao" && consoleInput.ToLower() != "não" && consoleInput.ToLower() != "n")
+                {
+                    Console.WriteLine("Não entendi, sou literal.");
+                }
+            }
+            else
+            {
+                Console.WriteLine(Program.ComoUsar());
+                foreach (var arq in orquestrador.Arquivos) Console.WriteLine("    " + arq.Value + Environment.NewLine);
+                Console.WriteLine(Environment.NewLine + "Se você está lendo esta mensagem é porque não foi encontrado nenhum arquivo.");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para fechar...");
+            Console.ReadLine();
+        }
+    }
+}
